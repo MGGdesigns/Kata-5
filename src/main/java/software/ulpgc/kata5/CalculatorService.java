e9894e752f0dd328793cbb23cf2caf95e6f4d787
Mauro Gómez <m.gomeznitoni@gmail.com>
2023-12-26 21:06:41 +0000

    CalculatorService class created

---
+++ src/main/java/software/ulpgc/kata5/CalculatorService.java
@@ -0,0 +1,38 @@
+package software.ulpgc.kata5;
+
+import spark.Request;
+
+public class CalculatorService {
+
+    public String getCalculatorPage() {
+        return "<html>" +
+                "<body>" +
+                "<h2>Factorial Calculator</h2>" +
+                "<form action='/calculate' method='post'>" +
+                "   Enter a number: <input type='text' name='number'>" +
+                "   <input type='submit' value='Calculate'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+
+    public String calculateFactorial(Request req) {
+        String numberParam = req.queryParams("number");
+        try {
+            int number = Integer.parseInt(numberParam);
+            long factorial = calculateFactorial(number);
+            return "Factorial of " + number + " is: " + factorial;
+        } catch (NumberFormatException e) {
+            return "Ivalid format.";
+        }
+    }
+
+    public long calculateFactorial(int number) {
+        if (number == 0 || number == 1) {
+            return 1;
+        } else {
+            return number * calculateFactorial(number-1);
+        }
+    }
+
+}
